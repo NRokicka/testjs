af68d8f00e73b2ad012285a7446865a5ddd32f0f
Natalia Rokicka <natalia9rokicka@gmail.com>
2017-01-26 22:22:17 +0000

    add file

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-//document.write('Hello world!');
 
+function equationDescr(a,b,c) {
+var d, e, x1, x2, result;
+d=b*b-4*a*c;
+e=Math.sqrt(d);
+if (a==0) {
+ result='a can not equal 0';  
+}
+else {
+if (d>0) {
+x1=(-b+e)/(2*a);
+x2=(-b-e)/(2*a);
+result="x1="+x1+", x2="+x2;
+}
+else if (d==0) {
+x1= -b/(2*a);
+result="x="+x1;
+}
+else if(d<0) {
+result='no result';
+}
+}
+return result;
+}
 
-var hello = 'Hello world!'
-alert(hello);
+alert(equationDescr(8,5,-60));
