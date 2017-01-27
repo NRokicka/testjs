21f84071c96fa1fe4f08ce2b8e6161ae9b92baef
Natalia Rokicka <natalia9rokicka@gmail.com>
2017-01-27 09:12:12 +0000

    add file

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
+var a=60;
+var b=5;
+var c=-60;
 
 function equationDescr(a,b,c) {
-var d, e, x1, x2, result;
+var d, x1, x2, result;
 d=b*b-4*a*c;
-e=Math.sqrt(d);
+
 if (a==0) {
  result='a can not equal 0';  
 }
 else {
 if (d>0) {
-x1=(-b+e)/(2*a);
-x2=(-b-e)/(2*a);
-result="x1="+x1+", x2="+x2;
+x1=(-b+Math.sqrt(d))/(2*a);
+x2=(-b-Math.sqrt(d))/(2*a);
+result='x1='+x1+', x2='+x2;
 }
 else if (d==0) {
 x1= -b/(2*a);
@@ -23,4 +26,4 @@ result='no result';
 return result;
 }
 
-alert(equationDescr(8,5,-60));
+document.write(equationDescr(a,b,c));
